82b1094ab5052fa2e74dd7def51dab916c4b951c
KannanDamodharan <kdamodh1@binghamton.edu>
2023-12-10 11:18:25 -0600

    Spring batch taskExecutor implementaion for improving performance

--- batch/src/main/java/com/varks/batch/config/StudentProcessor.java
+++ batch/src/main/java/com/varks/batch/config/StudentProcessor.java
@@ -8,6 +8,7 @@ public class StudentProcessor implements ItemProcessor<Student, Student> {
     @Override
     public Student process(Student student) throws Exception {
         //business logic goes here
+        student.setId(null);
         return student;
     }
 }
